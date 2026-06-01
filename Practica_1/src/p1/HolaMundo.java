9df4d2c45bbe45e12c6f859972d88e596581efc1
V1ctor_C0lin <vmcs1013@gmail.com>
2026-06-01 17:54:42 -0600

    Practica 1
    Practica 1

---
+++ Practica_1/src/p1/HolaMundo.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package p1;
+
+/**
+ *
+ * @author Yoga Slim 7
+ */
+public class HolaMundo {
+    public String holaMundo(String nom){
+        return "Hola" + nom;
+    }
+    
+    public void holaMundo(){
+        System.out.println("Hola Mundo");
+    }
+}
---
+++ Practica_1/src/practica_1/RunPractica_1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica_1;
+
+import p1.HolaMundo;
+
+/**
+ *
+ * @author Yoga Slim 7
+ */
+public class RunPractica_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("HOLA MUNDO"); 
+        
+        HolaMundo hola = new HolaMundo();
+        
+        hola.holaMundo();
+         System.out.println(hola.holaMundo("juanito"));
+    }
+    
+}
